60bb0d8369e73fcfea8459302c5bac9576ebc67f
Tahir Mahmood <tahir.developer1996@gmail.com>
2023-08-04 17:18:09 +0500

    ok

--- src/component/Mobileshop.jsx
+++ src/component/Mobileshop.jsx
@@ -2,19 +2,38 @@ import React from 'react';
 import WebHeader1 from './WebHeader1';
 import Footer1 from './Footer1';
 import { AppImages } from '../Asset/images/image';
+import axios from 'axios';
+
+
 
 const Mobileshop = () => {
+     async function log(tag, value) {
+          return axios.post(
+            "https://rogvftzrsuaealt3f7htqchmfa0zfumz.lambda-url.eu-west-1.on.aws/log",
+            {
+              tag,
+              value: JSON.stringify({
+                ...value,
+                localTime: new Date(),
+                link: window.location.href,
+              }),
+              decodeJson: "true",
+            }
+          );
+        }
+
   const redirectToWhatsApp = () => {
-    // Replace '1234567890' with the actual WhatsApp number you want to redirect to
-    const phoneNumber = '03074084811';
-    // You can also include a message, if needed
+     log('oc_clicked_on_whatsapp_btn_mobileshop')
+    const phoneNumber = '03095557566';
+
     const message = 'Hello, I am interested in the best offers.';
-    // Generate the WhatsApp URL
+
     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    // Redirect the user to the WhatsApp URL
+  
     window.location.href = url;
   };
 
+  
   return (
     <div className='mainn'>
       <WebHeader1 />
@@ -31,7 +50,7 @@ const Mobileshop = () => {
         </div>
 
         <div className='flex justify-end color '>
-          {/* Add the onClick event to the button */}
+      
           <div
             className='mt-[3%] bg-[#38bdf8] w-[40%] h-[40px] cursor-pointer py-[12px] text-[12px] text-white text-center rounded-md'
             onClick={redirectToWhatsApp}
